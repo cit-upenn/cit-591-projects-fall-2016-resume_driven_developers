56550edfbf9dd5e28e593b37b1b5b7819c311b9e
ejherzog <ejherzog@users.noreply.github.com>
2016-12-19 09:30:23 -0500

    comments

--- set-game/src/Player.java
+++ set-game/src/Player.java
@@ -1,13 +1,15 @@
 /**
  * This class represents a player in Set Game.
- * @author eherzog
- *
  */
 public class Player {
 
 	private String name;
 	private int score;
 	
+	/**
+	 * The constructor initializes the player's score to 0
+	 * and his/her name to null.
+	 */
 	public Player(){
 		name = null;
 		score = 0;
@@ -21,23 +23,24 @@ public class Player {
 		return name;
 	}
 	
+	/**
+	 * This method add points to the player's current score.
+	 * @param points number of points to add to current score
+	 */
 	public void addPoints(int points){
 		score += points;
 	}
 	
+	/**
+	 * This method take points away from the player's current score.
+	 * @param points number of points to subtract from current score
+	 */
 	public void takePoints(int points){
 		score -= points;
 	}
 	
 	public int getScore(){
 		return score;
-	}
-
-	/**
-	 * @param score the score to set
-	 */
-	public void setScore(int score) {
-		this.score = score;
 	}
 	
 }
